d6237b525465e8191903a4b43f3d32d2b9058933
proPatrick14 <learningemail.patrick@gmail.com>
2025-06-09 23:43:44 -0400

    Added initial values, added basic effect actor to change a value

---
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -0,0 +1,31 @@
+// Copyright Floral Dragon Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "AuraEffectActor.generated.h"
+
+UCLASS()
+class AURA_API AAuraEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AAuraEffectActor();
+
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	TObjectPtr<USphereComponent> Sphere;
+
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
